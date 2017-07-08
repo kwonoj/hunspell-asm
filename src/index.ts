d1008bcf2e822730a4c1dc3bd9b1ce293d28346c
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-07 23:56:56 -0700

    feat(Hunspell): define initial interface

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-/*let hunspellLoader;
-if (typeof module !== 'undefined' && module.exports) {
-  hunspellLoader = import('./node/index');
-} else {
-
-}*/
-
+import { spellCheckerFactory } from './Hunspell';
+import { hunspellLoader } from './hunspellLoader';
 import { isWasmEnabled } from './util/isWasmEnabled';
 
-export const loadModule = async () => {
+export const loadModule = async (): Promise<spellCheckerFactory> => {
   //tslint:disable-next-line:no-require-imports
-  const hunspellModule = isWasmEnabled() ? require('./lib/wasm/hunspell') : null;
-  console.log(hunspellModule);
+  const moduleLoader = isWasmEnabled() ? require('./lib/wasm/hunspell') : null;
+  const asmModule = moduleLoader();
+
+  //TODO: need to move into preprocessor
+  const initialize = new Promise((resolve) => asmModule.onRuntimeInitialized = () => resolve());
+  await initialize;
+
+  return hunspellLoader(asmModule);
 };
