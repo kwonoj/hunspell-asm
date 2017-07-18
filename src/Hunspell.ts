c74c929811bcb33f6b57748a263fe1a0890dc4fc
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-18 14:04:51 -0700

    refactor(HunspellFactory): revise interface

--- src/Hunspell.ts
+++ src/Hunspell.ts
@@ -1,7 +1,13 @@
-export type spellCheckerFactory = (dictionaryPath: string) => Hunspell;
-
 export interface Hunspell {
-  spell(word: string): boolean;
-  suggest(word: string): Array<string>;
-  dispose(): void;
+  dispose: () => void;
+  spell: (word: string) => boolean;
+  suggest: (word: string) => Array<string>;
+}
+
+export interface HunspellFactory {
+  mountDirectory: (dirPath: string) => string;
+  mountBuffer: (contents: Uint8Array, fileName: string) => string;
+  unmount: (mountedFilePath: string) => void;
+
+  create: (affPath: string, dictPath: string) => Hunspell;
 }
