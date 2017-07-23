6a303e1b1946d0357c2ed8eda5362b86ad58fc0f
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-22 19:08:01 -0700

    docs(example): add nodejs examples

---
+++ examples/node_buffer/index.ts
@@ -0,0 +1,44 @@
+//tslint:disable:no-console
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as Rx from 'rxjs';
+import { loadModule } from '../../src';
+
+const readFile = Rx.Observable.bindNodeCallback(fs.readFile);
+
+const dictPath = path.resolve('../dictionary');
+const misSpelledWord = '들어오세';
+const correctWord = '들어오세요';
+
+const runHunspell = async () => {
+  const hunspellFactory = await loadModule();
+
+  const affBuffer = await readFile(path.join(dictPath, 'korean.aff')).toPromise();
+  const dicBuffer = await readFile(path.join(dictPath, 'korean.dic')).toPromise();
+
+  const affFile = hunspellFactory.mountBuffer(affBuffer, 'korean.aff');
+  const dictFile = hunspellFactory.mountBuffer(dicBuffer, 'korean.dic');
+
+  console.log(`mounted aff to '${affFile}'`);
+  console.log(`mounted dic to '${dictFile}'`);
+
+  const hunspell = hunspellFactory.create(affFile, dictFile);
+
+  const misSpell = hunspell.spell(misSpelledWord);
+  console.log(`check spell for word '${misSpelledWord}': ${misSpell}`);
+  assert(misSpell === false);
+
+  const correctSpell = hunspell.spell(correctWord);
+  console.log(`check spell for word '${correctWord}': ${correctSpell}`);
+  assert(correctSpell === true);
+
+  const suggestion = hunspell.suggest(misSpelledWord);
+  console.log(`spell suggestion for misspelled word '${misSpelledWord}': ${suggestion}`);
+
+  hunspell.dispose();
+  hunspellFactory.unmount(affFile);
+  hunspellFactory.unmount(dictFile);
+};
+
+runHunspell();
---
+++ examples/node_fs/index.ts
@@ -0,0 +1,37 @@
+//tslint:disable:no-console
+import * as assert from 'assert';
+import * as path from 'path';
+import * as unixify from 'unixify';
+import { loadModule } from '../../src';
+
+const dictPath = path.resolve('../dictionary');
+const misSpelledWord = '들어오세';
+const correctWord = '들어오세요';
+
+const runHunspell = async () => {
+  const hunspellFactory = await loadModule();
+  const mountedPath = hunspellFactory.mountDirectory(dictPath);
+
+  console.log(`path '${dictPath}' mounted to '${mountedPath}'`);
+
+  const dictFile = unixify(path.join(mountedPath, 'korean.dic'));
+  const affFile = unixify(path.join(mountedPath, 'korean.aff'));
+
+  const hunspell = hunspellFactory.create(affFile, dictFile);
+
+  const misSpell = hunspell.spell(misSpelledWord);
+  console.log(`check spell for word '${misSpelledWord}': ${misSpell}`);
+  assert(misSpell === false);
+
+  const correctSpell = hunspell.spell(correctWord);
+  console.log(`check spell for word '${correctWord}': ${correctSpell}`);
+  assert(correctSpell === true);
+
+  const suggestion = hunspell.suggest(misSpelledWord);
+  console.log(`spell suggestion for misspelled word '${misSpelledWord}': ${suggestion}`);
+
+  hunspell.dispose();
+  hunspellFactory.unmount(mountedPath);
+};
+
+runHunspell();
