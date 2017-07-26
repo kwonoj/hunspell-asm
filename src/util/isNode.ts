19872ef784294c5239974685c7ab0e5f55b6728c
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-25 20:19:26 -0700

    fix(isNode): detect node.js via process.versions

---
+++ src/util/isNode.ts
@@ -0,0 +1,11 @@
+//naïve detection for running environment
+export const isNode = () => {
+  if (typeof process === 'object') {
+    if (typeof process.versions === 'object') {
+      if (typeof process.versions.node !== 'undefined') {
+        return true;
+      }
+    }
+  }
+  return false;
+};
