cfa122067fde9b97778642ea81ecbfcb7b7b6836
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-25 17:23:45 -0700

    docs(example): refactor code to reuse logics

--- examples/node_fs/index.ts
+++ examples/node_fs/index.ts
@@ -1,14 +1,12 @@
 //tslint:disable:no-console
-import * as assert from 'assert';
 import * as path from 'path';
 import * as unixify from 'unixify';
 import { loadModule } from '../../src';
+import { runHunspell } from '../runHunspell';
 
-const dictPath = path.resolve('./');
-const misSpelledWord = '들어오세';
-const correctWord = '들어오세요';
+const dictPath = path.resolve('../../spec/__fixtures__');
 
-const runHunspell = async () => {
+const runNodeFileHunspell = async () => {
   const hunspellFactory = await loadModule();
   const mountedPath = hunspellFactory.mountDirectory(dictPath);
 
@@ -17,23 +15,9 @@ const runHunspell = async () => {
   const dictFile = unixify(path.join(mountedPath, 'korean.dic'));
   const affFile = unixify(path.join(mountedPath, 'korean.aff'));
 
-  const hunspell = hunspellFactory.create(affFile, dictFile);
+  runHunspell(hunspellFactory, affFile, dictFile);
 
-  const misSpell = hunspell.spell(misSpelledWord);
-  console.log(`check spell for word '${misSpelledWord}': ${misSpell}`);
-  assert(misSpell === false);
-
-  const correctSpell = hunspell.spell(correctWord);
-  console.log(`check spell for word '${correctWord}': ${correctSpell}`);
-  assert(correctSpell === true);
-
-  const suggestion = hunspell.suggest(misSpelledWord);
-  console.log(`spell suggestion for misspelled word '${misSpelledWord}': ${suggestion}`);
-  assert(suggestion.length === 1);
-  assert(suggestion[0] === correctWord);
-
-  hunspell.dispose();
   hunspellFactory.unmount(mountedPath);
 };
 
-runHunspell();
+runNodeFileHunspell();
