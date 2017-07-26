3f5bcbda3cea927672c06dc22286bbbce97dd195
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-25 21:43:53 -0700

    docs(example): add example for browser

---
+++ examples/browser/index.ts
@@ -0,0 +1,26 @@
+//tslint:disable:no-console
+import { loadModule } from '../../src/index';
+import { enableLogger } from '../../src/logger';
+import { isWasmEnabled } from '../../src/util/isWasmEnabled';
+import { runHunspell } from '../runHunspell';
+
+enableLogger(console.log.bind(console));
+
+const runBrowserHunspell = async () => {
+  const hunspellFactory = await loadModule(`../../src/lib/${isWasmEnabled() ? 'wasm' : 'asm'}`);
+
+  const aff = await fetch('../../spec/__fixtures__/korean.aff');
+  const affBuffer = new Uint8Array(await aff.arrayBuffer());
+  const affFile = hunspellFactory.mountBuffer(affBuffer, 'korean.aff');
+
+  const dic = await fetch('../../spec/__fixtures__/korean.dic');
+  const dicBuffer = new Uint8Array(await dic.arrayBuffer());
+  const dictFile = hunspellFactory.mountBuffer(dicBuffer, 'korean.dic');
+
+  runHunspell(hunspellFactory, affFile, dictFile);
+
+  hunspellFactory.unmount(affFile);
+  hunspellFactory.unmount(dictFile);
+};
+
+runBrowserHunspell();
