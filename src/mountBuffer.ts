2b1cbb21ad6584f21962ab64ab3ef7329421b56d
OJ Kwon <kwon.ohjoong@gmail.com>
2017-07-27 19:51:27 -0700

    refactor(mountBuffer): split mountBuffer into module

---
+++ src/mountBuffer.ts
@@ -0,0 +1,31 @@
+import * as cuid from 'cuid';
+import { FS } from './HunspellAsmModule';
+import { isMounted } from './isMounted';
+import { log } from './util/logger';
+
+/**
+ * Creates a function to mount contents of file into wasm internal memory filesystem
+ * to allow wasm can access.
+ *
+ * @param {FS} FS wasm module filesystem
+ * @param {string} memPathId root path in memory filesystem to mount given arrayBuffer.
+ * This prefix path is generated automatically each time wasm module is loaded.
+ *
+ * @return {(contents: ArrayBufferView, fileName?: string) => string} function to mount buffer under memory filesystem.
+ * If filename is not provided, it'll be generated automatically. This function checks existing file mounted via filename,
+ * does not validate contents of buffer to find out already mounted one.
+ */
+
+/** @internal */
+export const mountBuffer = (FS: FS, memPathId: string) => (contents: ArrayBufferView, fileName?: string): string => {
+  const file = fileName || cuid();
+  const mountedFilePath = `${memPathId}/${file}`;
+
+  if (isMounted(FS, mountedFilePath, 'file')) {
+    log(`mountTypedArrayFile: file is already mounted, return it`);
+  } else {
+    FS.writeFile(mountedFilePath, contents, { encoding: 'binary' });
+  }
+
+  return mountedFilePath;
+};
