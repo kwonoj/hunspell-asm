39ec72539121ccdffaafc8b309b36de7d25cfed8
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-09 19:00:00 -0700

    feat(index): export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { loadModule } from './loadModule';
+export { HunspellFactory } from './HunspellFactory';
+export { Hunspell } from './Hunspell';
 export { log, enableLogger } from './util/logger';
