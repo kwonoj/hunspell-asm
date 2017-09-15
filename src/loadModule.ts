110103ffe2d82845900125bcbe3ae1723fd51fe4
OJ Kwon <kwon.ohjoong@gmail.com>
2017-09-14 22:55:52 -0700

    refactor(loadmodule): refactor to use getmoduleloader

--- src/loadModule.ts
+++ src/loadModule.ts
@@ -1,51 +1,24 @@
-import { ENVIRONMENT } from './environment';
+import { ENVIRONMENT, getModuleLoader, isNode, isWasmEnabled } from 'emscripten-wasm-loader';
+import { HunspellAsmModule } from './HunspellAsmModule';
 import { HunspellFactory } from './HunspellFactory';
 import { hunspellLoader } from './hunspellLoader';
-import { isNode } from './util/isNode';
-import { isWasmEnabled } from './util/isWasmEnabled';
 import { log } from './util/logger';
 
-/**
- * Asynchronously load and initialize asm module.
- *
- * @param {string} [binaryEndpoint] Provides endpoint to server to download binary module
- * (.wasm, .mem) via fetch when initialize module in a browser environment.
- * @param {ENVIRONMENT} [environment] Override running environment to load binary module.
- * This option is mostly for Electron's renderer process, which is detected as node.js env by default
- * but in case of would like to use fetch to download binary module.
- *
- * @returns {HunspellFactory} Factory function manages lifecycle of hunspell and virtual files.
- */
-export const loadModule = async (binaryEndpoint?: string, environment?: ENVIRONMENT): Promise<HunspellFactory> => {
-  const asmType = isWasmEnabled() ? 'wasm' : 'asm';
-  log(`loadModule: load hunspell module loader from `, asmType);
+const asmPath = `./lib/${isWasmEnabled() ? 'wasm' : 'asm'}`;
+log(`loadModule: load hunspell module loader from `, asmPath);
 
-  //tslint:disable-next-line:no-require-imports
-  const moduleLoader = require(`./lib/${asmType}/hunspell`);
-  log(`loadModule: moduleLoader imported`);
+//imports MODULARIZED emscripten preamble
+//tslint:disable-next-line:no-require-imports no-var-requires
+const runtimeModule = require(`${asmPath}/hunspell`);
 
-  const env = environment ? environment : isNode() ? ENVIRONMENT.NODE : ENVIRONMENT.BROWSER;
-  log(`loadModule: ${environment ? `using environment override ${environment}` : `running environment is ${env}`}`);
-
-  if (!binaryEndpoint && env === ENVIRONMENT.BROWSER) {
-    throw new Error('Cannot download binary module without endpoint on browser');
+export const loadModule: (
+  binaryEndpoint?: string,
+  environment?: ENVIRONMENT
+) => Promise<HunspellFactory> = getModuleLoader<HunspellFactory, HunspellAsmModule>(
+  (runtime: HunspellAsmModule) => hunspellLoader(runtime),
+  {
+    //tslint:disable-next-line:no-require-imports
+    dir: isNode() ? require('path').dirname(require.resolve(`${asmPath}/hunspell`)) : null,
+    runtimeModule
   }
-
-  const module = { ENVIRONMENT: env };
-
-  const asmModule = !!binaryEndpoint
-    ? moduleLoader({
-        ...module,
-        locateFile: (fileName: string) =>
-          env === ENVIRONMENT.NODE
-            ? //tslint:disable-next-line:no-require-imports
-              require('path').join(binaryEndpoint, fileName)
-            : `${binaryEndpoint}/${fileName}`
-      })
-    : moduleLoader(module);
-
-  await asmModule.initializeRuntime();
-  log(`loadModule: initialized hunspell Runtime`);
-
-  return hunspellLoader(asmModule);
-};
+);
