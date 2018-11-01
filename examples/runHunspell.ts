9116218367b51e05e5c977e4250ad08e21680892
OJ Kwon <kwon.ohjoong@gmail.com>
2018-11-01 11:56:10 -0700

    docs(example): update examples

--- examples/runHunspell.ts
+++ examples/runHunspell.ts
@@ -1,7 +1,7 @@
 //tslint:disable:no-console
 import { HunspellFactory } from '../src/HunspellFactory';
-const misSpelledWord = '들어오세';
-const correctWord = '들어오세요';
+const misSpelledWord = '한녕하세요';
+const correctWord = '안녕하세요';
 
 const runHunspell = (hunspellFactory: HunspellFactory, affPath: string, dicPath: string) => {
   const hunspell = hunspellFactory.create(affPath, dicPath);
@@ -16,7 +16,6 @@ const runHunspell = (hunspellFactory: HunspellFactory, affPath: string, dicPath:
 
   const suggestion = hunspell.suggest(misSpelledWord);
   console.log(`spell suggestion for misspelled word '${misSpelledWord}': ${suggestion}`);
-  console.assert(suggestion.length === 1);
   console.assert(suggestion[0] === correctWord);
 
   hunspell.dispose();
