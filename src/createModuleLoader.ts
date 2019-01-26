e6ee3ac3666b882c560d661d8e4e98309aeb1f65
OJ Kwon <kwon.ohjoong@gmail.com>
2019-01-25 23:06:35 -0800

    fix(createmoduleloader): apply overridden env

--- src/createModuleLoader.ts
+++ src/createModuleLoader.ts
@@ -23,14 +23,19 @@ const createModuleLoader = async (
   //tslint:disable-next-line:no-require-imports no-var-requires
   const lookupBinary = locateBinary || ((_filePath: string) => require('./lib/hunspell.wasm'));
 
+  //https://github.com/kwonoj/docker-hunspell-wasm/issues/63
+  //apply overridden environment values to custom patched hunspell preamble.
+  const baseModule = { ENVIRONMENT: env };
+
   //Build module object to construct wasm binary module via emscripten preamble.
   //This allows to override default wasm binary resolution in preamble.
   //By default, hunspell-asm overrides to direct require to binary on *browser* environment to allow bundler like webpack resolves it.
   //On node, it relies on default resolution logic.
   const overriddenModule =
-    isNode() && !locateBinary
-      ? undefined
+    env === ENVIRONMENT.NODE && !locateBinary
+      ? baseModule
       : {
+          ...baseModule,
           locateFile: (filePath: string) => (filePath.endsWith('.wasm') ? lookupBinary(filePath) : filePath)
         };
 
